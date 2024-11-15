c263ea1e9074f60ffb89faa140c632f6071b9a8c
Ritsuki.Y <42468908+R-second@users.noreply.github.com>
2024-11-15 07:10:14 +0000

    refac: utils.pyのparams

--- python/ukfm/utils.py
+++ python/ukfm/utils.py
@@ -9,6 +9,5 @@ def set_matplotlib_config():
     matplotlib.rcParams['legend.fontsize'] = 'x-large'
     matplotlib.rcParams['xtick.labelsize'] = 'x-large'
     matplotlib.rcParams['ytick.labelsize'] = 'x-large'
-    params= {'text.latex.preamble' : [r'\usepackage{amsmath}', 
-        r'\usepackage{amssymb}']}
+    params= {'text.latex.preamble' : r'\usepackage{amsmath} \usepackage{amssymb}'}
     matplotlib.rcParams.update(params)
